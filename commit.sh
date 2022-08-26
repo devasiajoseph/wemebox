16dbdc5925c73af0619457ce31832a04a6869871
Devasia Joseph <devasiajoseph@gmail.com>
2022-08-26 17:40:49 +0530

    one commit

--- commit.sh
+++ commit.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-# Toptech commit script
+# Wemebox commit script
 
 git add --all
 git commit -m "$1"
 git push origin master
+
+cd sites
+
+git add --all
+git commit -m "$1"
+git push origin main
+
