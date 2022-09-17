9278d7066e3b800aa2a1317ed7283542809e7b73
Devasia Joseph <devasiajoseph@gmail.com>
2022-09-17 19:32:06 +0530

    migrate

--- sql/migrate.sql
+++ sql/migrate.sql
@@ -1,3 +1,4 @@
 alter table domain add unique(domain_dir);
 alter table domain add unique(domain_name);
 alter table domain add active BOOLEAN not null default true;
+alter table user_account add column  role VARCHAR(10) not null default 'user';
